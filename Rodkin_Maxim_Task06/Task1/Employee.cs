01b2f461ca632bcf42f901d0b3fabd560b6daa7c
MaximRodkin <flash.l2@yandex.ru>
2018-10-15 15:45:47 +0300

    Add task06

---
+++ Rodkin_Maxim_Task06/Task1/Employee.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Employee:User
+    {
+        private DateTime employmentDate;
+        public DateTime EmploymentDate
+        {
+            get
+            {
+                return employmentDate;
+            }
+            private set
+            {
+                employmentDate = value;
+            }
+        }
+        private string post;
+
+        public string Post
+        {
+            get
+            {
+                return post;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    ValidateString(ref post, value, "Incorrect post");
+                }
+                else
+                {
+                    throw new Exception("Incorrect post");
+                }
+            }
+        }
+
+        public int WorkExperience
+        {
+            get
+            {
+                var days = DateTime.Now - employmentDate;
+                return days.Days / 365;
+            }
+            private set
+            {
+
+            }
+        }
+
+        public Employee(string firstName, string lastName, DateTime birthday, string post, DateTime employmentDate):base(firstName, lastName, birthday)
+        {
+            Post = post;
+            EmploymentDate = employmentDate;
+        }
+
+        public Employee(string firstName, string lastName, string middleName, DateTime birthday, string post, DateTime employmentDate):base(firstName, lastName, middleName, birthday)
+        {
+            Post = post;
+            EmploymentDate = employmentDate;
+        }
+    }
+}
---
+++ Rodkin_Maxim_Task06/Task2/Round.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Task2
+{
+    class Round
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        private double radius;
+
+        public Round(int x, int y, double radius)
+        {
+            X = x;
+            Y = y;
+            Radius = radius;
+        }
+
+        public Round(int x, int y) : this(x, y, 1)
+        {
+
+        }
+
+        public Round() : this(0, 0, 1)
+        {
+
+        }
+
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+
+            set
+            {
+                if (value > 0)
+                {
+                    radius = value;
+                }
+                else
+                {
+                    throw new Exception("Incorrect radius");
+                }
+            }
+        }
+
+        public double Area
+        {
+            get
+            {
+                return Math.PI * Math.Pow(radius, 2);
+            }
+        }
+
+        public double Length
+        {
+            get
+            {
+                return Math.PI * 2 * radius;
+            }
+        }
+    }
+}
