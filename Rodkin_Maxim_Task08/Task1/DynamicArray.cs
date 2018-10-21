d11b926ed601f026254e9285040a85d19309fdf2
MaximRodkin <flash.l2@yandex.ru>
2018-10-21 19:23:05 +0300

    add Rodkin_Maxim_Task08

---
+++ Rodkin_Maxim_Task08/Task1/DynamicArray.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class DynamicArray<T> where T: IComparable<T>, new()
+    {
+        private T[] array;
+        public int Length { get; private set; }
+        public int Capacity
+        {
+            get
+            {
+                return array.Length;
+            }
+        }
+
+        public DynamicArray():this(8)
+        {
+
+        }
+
+        public DynamicArray(int size)
+        {
+            array = new T[size];
+        }
+
+        public DynamicArray(T[] inputArray)
+        {
+            Array.Copy(inputArray, array, inputArray.Length);
+            Length = inputArray.Length;
+        }
+
+        public void Add(T item)
+        {
+            if (Capacity <= Length)
+            {
+                Array.Resize(ref array, Capacity * 2);
+            }
+
+            array[Length] = item;
+            Length++;
+        }
+
+        public void AddRange(T[] inputArray)
+        {
+            if (inputArray.Length > Capacity-Length)
+            {
+                int range = array.Length;
+                do
+                {
+                    range *= 2;
+                } while (inputArray.Length > range - Length);
+
+                Array.Resize(ref array, range);
+            }
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                Add(inputArray[i]);
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                if (array[i].CompareTo(item)==0)
+                {
+                    for (int j = i; j < Length-1; j++)
+                    {
+                        array[j] = array[j + 1];
+                    }
+                    array[Length-1] = default(T);
+                    Length--;
+                    break;
+                }
+            }
+            return true;
+        }
+
+        public bool Insert(T item, int id)
+        {
+            if (id <= Length)
+            {
+                if (Capacity <= Length)
+                {
+                    Array.Resize(ref array, Capacity * 2);
+                }
+                for (int i = Length; i > id; i--)
+                {
+                    array[i] = array[i - 1];
+                }
+                array[id] = item;
+                Length++;
+                return true;
+            }
+            else
+            {
+
+                throw new ArgumentOutOfRangeException();
+
+            }
+        }
+
+
+        public T this[int index]
+        {
+
+            get
+            {
+                if (index >= Capacity)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else return array[index];
+            }
+            set
+            {
+                if (index >= Capacity)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else array[index] = value;
+            }
+        }
+    }
+}
---
+++ Rodkin_Maxim_Task08/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicArray<int> test1 = new DynamicArray<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                test1.Add(i);
+            }
+            for (int i = 0; i < test1.Capacity; i++)
+            {
+                Console.WriteLine(test1[i]);
+            }
+            Console.WriteLine();
+            test1.Insert(8, 3);
+            for (int i = 0; i < test1.Capacity; i++)
+            {
+                Console.WriteLine(test1[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
